e0ff71104b3b276998a3c7e37166dce8258228e0
DocPep <kamblevineeth@gmail.com>
2022-06-24 16:58:51 +0530

    messagecard

--- live-chat/pages/components/messagecard/messagecard.js
+++ live-chat/pages/components/messagecard/messagecard.js
@@ -1,5 +1,7 @@
 import Grid from "@mui/material/Grid";
+import Avatar from '@mui/material/Avatar';
 import React from "react";
+import {deepOrange, red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, brown, grey} from '@mui/material/colors';
 class MessageCard extends React.Component {
     constructor(props){
         super(props);
@@ -7,17 +9,20 @@ class MessageCard extends React.Component {
     render(){
         console.log(this.props);
         console.log("props");
+        const AvatarColor = [deepOrange, pink, purple, red, indigo, deepPurple, cyan, green, blue, grey, amber, lightBlue, lightGreen, teal, lime, brown, yellow, amber];
+        const ColorIndex = (((this.props.sender).length)+Math.floor(Math.random() * 19))%18;
+        const Sender = (this.props.sender).slice(0,1).toUpperCase();
         return (
             <>
-                <Grid container maxWidth sx={{border: "2px solid black"}}>
+                <Grid container maxWidth >
                     <Grid item xs={12} sx={{display: "flex"}} justifyContent="right">
-                        <b>{this.props.sender?this.props.sender:"123"}</b>
+                        <Avatar sx={{bgcolor: AvatarColor[ColorIndex][500], height: 30, width: 30}}>{Sender}</Avatar>
                     </Grid>
                     <Grid item xs={12} sx={{display: "flex"}} justifyContent="right">
-                        {this.props.message?this.props.message:"123"}
+                        {this.props.message}
                     </Grid>
-                    <Grid item xs={12} sx={{display: "flex"}} justifyContent="right">
-                        <b>{this.props.time?this.props.time:"123"}</b>
+                    <Grid item xs={12} sx={{display: "flex"}} justifyContent="right" fontSize={9}>
+                        <b>{this.props.time}</b>
                     </Grid>
                 </Grid>
             </>
