a7e878d462de4729e643ae749cea8917d0a8c4af
Gourav1100 <gauravb1100@gmail.com>
2022-06-24 19:34:43 +0530

    vercel fix

--- live-chat/pages/components/messagecard/messagecard.js
+++ live-chat/pages/components/messagecard/messagecard.js
@@ -3,26 +3,19 @@ import Avatar from '@mui/material/Avatar';
 import React from "react";
 import {deepOrange, red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, brown, grey} from '@mui/material/colors';
 class MessageCard extends React.Component {
+    AvatarColor = [deepOrange, pink, purple, red, indigo, deepPurple, cyan, green, blue, grey, amber, lightBlue, lightGreen, teal, lime, brown, yellow, amber];
     constructor(props){
         super(props);
+        this.setState({
+            ColorIndex: 0,
+        })
     }
     render(){
-        const AvatarColor = [deepOrange, pink, purple, red, indigo, deepPurple, cyan, green, blue, grey, amber, lightBlue, lightGreen, teal, lime, brown, yellow, amber];
-        let ColorIndex = "";
-        if(sessionStorage.getItem(this.props.sender) !== null){
-            ColorIndex = sessionStorage.getItem(this.props.sender);
-        }
-        else{
-            ColorIndex = ((this.props.sender?this.props.sender.length:0)+Math.floor(Math.random()*19))%18;
-            if(this.props.sender){
-                sessionStorage.setItem(this.props.sender, ColorIndex);
-            }
-        }
         return (
             <>
                 <Grid container maxWidth >
                     <Grid item xs={1} sx={{display: "flex", padding: 1}} justifyContent="center">
-                        <Avatar sx={{bgcolor: AvatarColor[ColorIndex][500]}}>{this.props.sender?this.props.sender.slice(0,1).toUpperCase():""}</Avatar>
+                        <Avatar sx={{bgcolor: AvatarColor[this.state.ColorIndex][500]}}>{this.props.sender?this.props.sender.slice(0,1).toUpperCase():""}</Avatar>
                     </Grid>
                     <Grid item xs={11} sx={{display: "flex", padding:2, fontWeight: 500, fontSize: 18}} justifyContent="left">
                         {this.props.message?this.props.message:""}
@@ -33,6 +26,22 @@ class MessageCard extends React.Component {
                 </Grid>
             </>
         );
+    }
+    componentDidMount(){
+        if(sessionStorage.getItem(this.props.sender) !== null){
+            this.setState({
+                ColorIndex: sessionStorage.getItem(this.props.sender),
+            });
+        }
+        else{
+            const ColorIndex = ((this.props.sender?this.props.sender.length:0)+Math.floor(Math.random()*19))%18;
+            if(this.props.sender){
+                sessionStorage.setItem(this.props.sender, ColorIndex);
+            }
+            this.setState({
+                ColorIndex: ColorIndex,
+            });
+        }
     }
 }
 
