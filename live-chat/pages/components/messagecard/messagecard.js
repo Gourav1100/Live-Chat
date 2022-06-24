429a0a17b18e2e29a1610c8aa43fd9377c04f79e
Gourav1100 <gauravb1100@gmail.com>
2022-06-24 19:37:15 +0530

    vercel fix

--- live-chat/pages/components/messagecard/messagecard.js
+++ live-chat/pages/components/messagecard/messagecard.js
@@ -3,19 +3,19 @@ import Avatar from '@mui/material/Avatar';
 import React from "react";
 import {deepOrange, red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, brown, grey} from '@mui/material/colors';
 class MessageCard extends React.Component {
-    AvatarColor = [deepOrange, pink, purple, red, indigo, deepPurple, cyan, green, blue, grey, amber, lightBlue, lightGreen, teal, lime, brown, yellow, amber];
     constructor(props){
         super(props);
         this.setState({
             ColorIndex: 0,
         })
+        this.AvatarColor = [deepOrange, pink, purple, red, indigo, deepPurple, cyan, green, blue, grey, amber, lightBlue, lightGreen, teal, lime, brown, yellow, amber];
     }
     render(){
         return (
             <>
                 <Grid container maxWidth >
                     <Grid item xs={1} sx={{display: "flex", padding: 1}} justifyContent="center">
-                        <Avatar sx={{bgcolor: AvatarColor[this.state.ColorIndex][500]}}>{this.props.sender?this.props.sender.slice(0,1).toUpperCase():""}</Avatar>
+                        <Avatar sx={{bgcolor: this.AvatarColor[this.state.ColorIndex][500]}}>{this.props.sender?this.props.sender.slice(0,1).toUpperCase():""}</Avatar>
                     </Grid>
                     <Grid item xs={11} sx={{display: "flex", padding:2, fontWeight: 500, fontSize: 18}} justifyContent="left">
                         {this.props.message?this.props.message:""}
